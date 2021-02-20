43e27bb2471abbcd5fe1b4e875a5c633c042249b
Alekhin Sergey <alekhin1991@gmail.com>
2021-02-20 11:51:07 +0300

    first public

---
+++ public/main.js
@@ -0,0 +1,8 @@
+console.log('Hello word!');
+
+const app = document.getElementById('app');
+
+
+const title = document.createElement(' div');
+title.innerHTML = '<div class=\'title\'>Hello word!</div>';
+app.appendChild(title);
