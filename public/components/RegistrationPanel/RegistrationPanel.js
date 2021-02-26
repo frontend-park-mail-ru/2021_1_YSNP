72de329d7c095314a5c3e8c5d6405b7e62801c5f
Ivan Gorshkov <ivan.a.gorshkov@gmail.com>
2021-02-26 17:58:01 +0300

    выполнена validation

--- public/components/RegistrationPanel/RegistrationPanel.js
+++ public/components/RegistrationPanel/RegistrationPanel.js
@@ -16,8 +16,9 @@ export class RegistrationPanel {
      * @this {RegistrationPanel}
      * @public
      */
-    constructor(parent) {
+    constructor(parent, data) {
         this.__parent = parent;
+        this.__data = data;
     }
 
     /***
@@ -27,6 +28,25 @@ export class RegistrationPanel {
      * @private
      * @this {RegistrationPanel}
      */
+
+
+    __drawForm() {
+        let fields = '';
+        for (const prop in this.__data) {
+            const element = this.__data[prop];
+            fields += `<div class="product--description">
+                <div class="product--description--topic">
+                    <p class="product--description--description__title">${element.title}</p>
+                </div>
+                <div class="product--description--description">
+                    <p class="product--description--description__text"><input id="${element.id}" type="${element.inputType}" placeholder="${element.placeholder}" name="${element.id}"/></p>
+                </div>
+            </div>`;
+        }
+
+        return fields;
+    }
+
     __getTemplate() {
         return `           
            <div class="reg-panel">
@@ -34,70 +54,48 @@ export class RegistrationPanel {
                    <p class="reg-panel--title__product-name">Регистрация</p>
                </div>
                
+               <form>
+            ${this.__drawForm()}
             <div class="product--description">
                 <div class="product--description--topic">
-                    <p class="product--description--description__title">Имя</p>
+                
                 </div>
                 <div class="product--description--description">
-                    <p class="product--description--description__text"> <input type="text" placeholder="Имя"/></p>
+                    <p class="product--description--description__text"> <input type="submit" id="register" value="Зарегистрироваться"/></p>
                 </div>
             </div>
             
-            <div class="product--description">
-                <div class="product--description--topic">
-                    <p class="product--description--description__title">Фамилия</p>
-                </div>
-                <div class="product--description--description">
-                    <p class="product--description--description__text"> <input type="text" placeholder="Фамилия"/></p>
-                </div>
-            </div>
-            
-            <div class="product--description">
-                <div class="product--description--topic">
-                    <p class="product--description--description__title">Телефон</p>
-                </div>
-                <div class="product--description--description">
-                    <p class="product--description--description__text"> <input type="text" placeholder="Телефон"/></p>
-                </div>
-            </div>
-            <div class="product--description">
-                <div class="product--description--topic">
-                    <p class="product--description--description__title">Почта</p>
-                </div>
-                <div class="product--description--description">
-                    <p class="product--description--description__text"> <input type="email" placeholder="Почта"/></p>
-                </div>
-            </div>
-            <div class="product--description">
-                <div class="product--description--topic">
-                    <p class="product--description--description__title">Пароль</p>
-                </div>
-                <div class="product--description--description">
-                    <p class="product--description--description__text"> <input type="password" placeholder="Пароль"/></p>
-                </div>
-            </div>
-            
-            <div class="product--description">
-                <div class="product--description--topic">
-                    <p class="product--description--description__title">Повторите пароль</p>
-                </div>
-                <div class="product--description--description">
-                    <p class="product--description--description__text"> <input type="password" placeholder="Пароль"/></p>
-                </div>
-            </div>
-            
-            
-            <div class="product--description">
-                <div class="product--description--topic">
-                    <p class="product--description--description__title">Дата рождения</p>
-                </div>
-                <div class="product--description--description">
-                    <p class="product--description--description__text"> <input type="date"/></p>
-                </div>
-            </div>
+               </form>
            </div>
         `;
     }
+
+
+    /***
+     * @author Ivan Gorshkov
+     *
+     * get Navigation listeners
+     * @this {RegistrationPanel}
+     * @private
+     * @readonly
+     * @return  {Object[]} array of listeners
+     */
+    get listeners() {
+        return this.__listeners;
+    }
+
+    /***
+     * @author Ivan Gorshkov
+     *
+     * Set new listeners
+     * @this {RegistrationPanel}
+     * @param  {Object[]} val - Object of listeners
+     * @public
+     */
+    set listeners(val) {
+        this.__listeners = val;
+    }
+
 
 
     /***
@@ -107,8 +105,23 @@ export class RegistrationPanel {
      * @this {RegistrationPanel}
      * @public
      */
+
+    addListeners() {
+        for (const prop in this.__data) {
+            const element = this.__data[prop];
+            document
+                .getElementById(element.id)
+                .addEventListener(element.listener.type, element.listener.listener);
+        }
+
+        document
+            .getElementById('register')
+            .addEventListener(this.listeners.register.type, this.listeners.register.listener);
+    }
+
     render() {
         const template = this.__getTemplate();
         this.__parent.insertAdjacentHTML('beforeend', template);
+        this.addListeners();
     }
 }
--- public/pages/Registration.js
+++ public/pages/Registration.js
@@ -1,3 +1,4 @@
+'use strict';
 
 import {Header} from '../components/Header/Header.js';
 import {Profile} from './Profile.js';
@@ -42,6 +43,25 @@ export class Registration {
         landing.render();
     }
 
+
+    listenerRegister(ev) {
+        ev.preventDefault();
+        const element1 = document.getElementById(this.__RegistrationForm().passwordConfirm.id);
+        const element2 = document.getElementById(this.__RegistrationForm().phone.id);
+        const element3 = document.getElementById(this.__RegistrationForm().password.id);
+        const element4 = document.getElementById(this.__RegistrationForm().mail.id);
+        const element5 = document.getElementById(this.__RegistrationForm().name.id);
+        const element6 = document.getElementById(this.__RegistrationForm().surname.id);
+        const element7 = document.getElementById(this.__RegistrationForm().date.id);
+        this._phone(element2);
+        this.__pas(element3);
+        this.__confPas(element1);
+        this.__mail(element4);
+        this.__empty(element5);
+        this.__empty(element6);
+        this.__empty(element7);
+    }
+
     /***
      * @author Ivan Gorshkov
      * func for create object of listeners
@@ -62,6 +82,237 @@ export class Registration {
                     type: 'click',
                     listener: this.listenerToBack.bind(this)
                 }
+            },
+            registration: {
+                register: {
+                    type: 'click',
+                    listener: this.listenerRegister.bind(this)
+                }
+            }
+        };
+    }
+
+
+    __isValidPhone(phoneNumber) {
+        var found = phoneNumber.search(/^(\+{0,})(\d{0,})([(]{1}\d{1,3}[)]{0,}){0,}(\s?\d+|\+\d{2,3}\s{1}\d+|\d+){1}[\s|-]?\d+([\s|-]?\d+){1,2}(\s){0,}$/);
+        return found > -1;
+    }
+
+    __validateEmail(email) {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
+
+    __checkPassword(inputtxt) {
+        var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+        return re.test(inputtxt);
+    }
+
+    listenerPhone(ev) {
+        ev.preventDefault();
+        this._phone(ev.target);
+    }
+
+    _phone(target) {
+        if (this.__isValidPhone(target.value)) {
+            target.style.border = '0.5px solid green';
+            target.parentNode.removeChild(target.nextSibling);
+        } else {
+            target.style.border = '0.5px solid red';
+            if (document.getElementById('phoneError') === null) {
+                const el = document.createElement('div');
+                el.id = 'phoneError';
+                el.innerHTML = `
+                        <ul>
+                          <li>Неверный формат телефона</li>
+                        </ul>
+                    `;
+                this.insertAfter(target, el);
+            }
+        }
+    }
+
+    __pas(target) {
+        if (this.__checkPassword(target.value)) {
+            target.style.border = '0.5px solid green';
+            target.parentNode.removeChild(target.nextSibling);
+        } else {
+            target.style.border = '0.5px solid red';
+            if (document.getElementById('passwordError') === null) {
+                const el = document.createElement('div');
+                el.id = 'passwordError';
+                el.innerHTML = `
+                        <ul>
+                          <li>От шести или более символов</li>
+                          <li>Содержит хотя бы одну цифру</li>
+                          <li>Хотя бы один символ нижнего регистра</li>
+                          <li>Хотя бы один символ верхнего регистра</li>
+                          <li>Только латинские символы</li>
+                        </ul>
+                    `;
+                this.insertAfter(target, el);
+            }
+        }
+    }
+
+    listenerPassWord(ev) {
+        ev.preventDefault();
+        this.__pas(ev.target);
+    }
+
+    insertAfter(referenceNode, newNode) {
+        referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    }
+
+
+    __confPas(target) {
+        const element = document.getElementById(this.__RegistrationForm().password.id);
+        if (element.value === target.value && target.value !== '') {
+            target.style.border = '0.5px solid green';
+            target.parentNode.removeChild(target.nextSibling);
+        } else {
+            target.style.border = '0.5px solid red';
+            if (document.getElementById('passwordConfirmError') === null) {
+                const el = document.createElement('div');
+                el.id = 'passwordConfirmError';
+                el.innerHTML = `
+                 <ul>
+                     <li>Пароли не совпадают</li>
+                 </ul>
+`;
+                this.insertAfter(target, el);
+            }
+        }
+    }
+
+    __mail(target) {
+        if (this.__validateEmail(target.value)) {
+            target.style.border = '0.5px solid green';
+            target.parentNode.removeChild(target.nextSibling);
+        } else {
+            target.style.border = '0.5px solid red';
+            if (document.getElementById('MailError') === null) {
+                const el = document.createElement('div');
+                el.id = 'MailError';
+                el.innerHTML = `
+                 <ul>
+                     <li>Неправильный формат mail</li>
+                 </ul>
+`;
+                this.insertAfter(target, el);
+            }
+        }
+    }
+
+    __empty(target) {
+        if (target.value !== '') {
+            target.style.border = '0.5px solid green';
+            target.parentNode.removeChild(target.nextSibling);
+        } else {
+            target.style.border = '0.5px solid red';
+            if (document.getElementById(`${target.id}Error`) === null) {
+                const el = document.createElement('div');
+                el.id = `${target.id}Error`;
+                el.innerHTML = `
+                 <ul>
+                     <li>Поле не должно быть пустым</li>
+                 </ul>
+`;
+                this.insertAfter(target, el);
+            }
+        }
+    }
+
+    listenerConfirmPassword(ev) {
+        ev.preventDefault();
+        this.__confPas(ev.target);
+    }
+
+    listenerMail(ev) {
+        ev.preventDefault();
+        if (this.__validateEmail(ev.target.value)) {
+            ev.target.style.border = '0.5px solid green';
+        } else {
+            ev.target.style.border = '0.5px solid red';
+        }
+    }
+
+    listenerName(ev) {
+        ev.preventDefault();
+        this.__empty(ev.target);
+    }
+
+    __RegistrationForm() {
+        return {
+            name: {
+                title: 'Имя',
+                placeholder: 'Имя',
+                inputType: 'text',
+                id: 'name',
+                listener: {
+                    type: 'input',
+                    listener: this.listenerName.bind(this)
+                }
+            },
+            surname: {
+                title: 'Фамилия',
+                placeholder: 'Фамилия',
+                inputType: 'text',
+                id: 'surname',
+                listener: {
+                    type: 'input',
+                    listener: this.listenerName.bind(this)
+                }
+            },
+            phone: {
+                title: 'Телефон',
+                placeholder: 'Телефон',
+                inputType: 'tel',
+                id: 'phone',
+                listener: {
+                    type: 'input',
+                    listener: this.listenerPhone.bind(this)
+                }
+            },
+            mail: {
+                title: 'Почта',
+                placeholder: 'Почта',
+                inputType: 'email',
+                id: 'mail',
+                listener: {
+                    type: 'input',
+                    listener: this.listenerMail.bind(this)
+                }
+            },
+            password: {
+                title: 'Пароль',
+                placeholder: 'Пароль',
+                inputType: 'password',
+                id: 'password',
+                listener: {
+                    type: 'change',
+                    listener: this.listenerPassWord.bind(this)
+                }
+            },
+            passwordConfirm: {
+                title: 'Повторите пароль',
+                placeholder: 'Пароль',
+                inputType: 'password',
+                id: 'passwordConfirm',
+                listener: {
+                    type: 'change',
+                    listener: this.listenerConfirmPassword.bind(this)
+                }
+            },
+            date: {
+                title: 'Дата рождения',
+                placeholder: null,
+                inputType: 'date',
+                id: 'date',
+                listener: {
+                    type: 'change',
+                    listener: this.listenerName.bind(this)
+                }
             }
         };
     }
@@ -73,11 +324,13 @@ export class Registration {
         this.__header.listeners = listeners.header;
         this.__header.render();
 
-        this.__navigation = new Navigation(this.__parent, 'В результаты поиска', {route: ['С пробегом', 'Mercedes-Benz']});
+        this.__navigation = new Navigation(this.__parent, 'Главная страница', {route: ['Регистрация профиля']});
         this.__navigation.listeners = listeners.backBtn;
         this.__navigation.render();
 
-        this.__registrationPanel = new RegistrationPanel(this.__parent);
+        const form = this.__RegistrationForm();
+        this.__registrationPanel = new RegistrationPanel(this.__parent, form);
+        this.__registrationPanel.listeners = listeners.registration;
         this.__registrationPanel.render();
     }
 }
