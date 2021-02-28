cf8b40fa77a2fcc100d469fba0f692a3d88799f4
Ivan Gorshkov <ivan.a.gorshkov@gmail.com>
2021-02-28 18:41:19 +0300

    добавил похожее

--- public/pages/Product.js
+++ public/pages/Product.js
@@ -8,6 +8,8 @@ import {NavigationController} from '../components/Navigation/NavigationControlle
 
 import {Board} from '../components/Board/Board.js';
 import {BoardController} from '../components/Board/BoardController.js';
+import {ProductList} from '../components/ProductList/ProductList.js';
+import {ProductListController} from '../components/ProductList/ProductListController.js';
 
 /***
  * @author Ivan Gorshkov
@@ -52,6 +54,47 @@ export class Product {
         };
     }
 
+    __getProductListData() {
+        return [
+            {
+                id: '1',
+                img: '/img/test-productCart.jpg',
+                name: 'Land Rover новый',
+                date: '9 февраля 12:07',
+                amount: '2 000 &#8381'
+            },
+            {
+                id: '2',
+                img: '/img/test-productCart.jpg',
+                name: 'Игровой пк',
+                date: '9 февраля 12:07',
+                amount: '422 000 &#8381'
+            },
+            {
+                id: '3',
+                img: '/img/test-productCart.jpg',
+                name: 'Дом прикольный',
+                date: '9 февраля 12:07',
+                amount: '760 000 &#8381'
+            },
+            {
+                id: '4',
+                img: '/img/test-productCart.jpg',
+                name: 'Apple iphone x',
+                date: '9 февраля 12:07',
+                amount: '43 000 &#8381'
+            },
+            {
+                id: '5',
+                img: '/img/test-productCart.jpg',
+                name: 'Honda Shadow Classic 400',
+                date: '9 февраля 12:07',
+                amount: '434 000 &#8381'
+            }
+        ];
+    }
+
+
     /***
      * @author Ivan Gorshkov
      *
@@ -77,5 +120,10 @@ export class Product {
         this.__board.render();
         this.__boardController = new BoardController(this.__parent, this.__board);
         this.__boardController.control();
+
+        const productList = new ProductList(this.__parent, this.__getProductListData());
+        productList.render();
+        this.__productListController = new ProductListController(this.productRemoveListeners.bind(this), this.__parent, productList);
+        this.__productListController.control();
     }
 }
