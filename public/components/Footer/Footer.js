74886fbfca27237b884b2982647259c3c4ce415c
Ivan Gorshkov <ivan.a.gorshkov@gmail.com>
2021-03-02 02:05:56 +0300

    Добавил JSDoc

--- public/components/Footer/Footer.js
+++ public/components/Footer/Footer.js
@@ -1,38 +1,67 @@
 'use strict';
 
+/***
+ * @author Ivan Gorshkov
+ * Footer class for render footer of web-app
+ * @class Footer
+ */
 export class Footer {
 
+    /***
+     * @author Ivan Gorshkov
+     *
+     * init of class Footer
+     * @param {HTMLElement} parent - parent element
+     * @constructor
+     * @this {Footer}
+     * @public
+     */
     constructor(parent) {
         this.__parent = parent;
     }
 
+    /***
+     * @author Ivan Gorshkov
+     *
+     * main template of component
+     * @return {string}
+     * @private
+     * @this {Footer}
+     */
     __getTemplate() {
         return `           
            <div class="footer">
              <hr class="footer-hr"/>
              <div class="footer-inner">
              <div class="footer-content">
-                <p>Помощь</p>
-                <p>Реклама на koya</p>
-                <p>Лицензионное соглашение</p>
+                <p class="footer__link">Помощь</p>
+                <p class="footer__link">Реклама на koya</p>
+                <p class="footer__link">Лицензионное соглашение</p>
              </div>
              <div class="footer-content">
-                <p>Безопасность</p>
-                <p>О компании</p>
+                <p class="footer__link">Безопасность</p>
+                <p class="footer__link">О компании</p>
              </div>
              <div class="footer-content">
-                <p>г. Москва</p>
-                <p>Профиль</p>
-                <p>Разместить объявление</p>
+                <p class="footer__link">г. Москва</p>
+                <p class="footer__link">Профиль</p>
+                <p class="footer__link">Разместить объявление</p>
              </div>
              <div class="footer-content">
-                <p>© 2021 YSNP</p>
+                <p class="footer__corp">© 2021 YSNP</p>
              </div>
              </div>
            </div>
         `;
     }
 
+    /***
+     * @author Ivan Gorshkov
+     *
+     * Add component to parent
+     * @this {Footer}
+     * @public
+     */
     render() {
         const template = this.__getTemplate();
         this.__parent.insertAdjacentHTML('beforeend', template);
