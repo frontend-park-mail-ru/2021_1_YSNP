af26562b34e48bbea04f1d18b7ee2a0b2be75519
Alekhin Sergey <alekhin1991@gmail.com>
2021-03-09 18:12:10 +0300

    fix bugs

--- public/main.js
+++ public/main.js
@@ -1,13 +1,8 @@
 'use strict';
 
-// import {Landing} from './pages/Landing.js';
-import {Profile} from './pages/Profile.js';
-import {Registration} from './pages/Registration.js';
+import {Landing} from './pages/Landing.js';
 
 const app = document.getElementById('app');
 
-// const registration = new Registration(app);
-// registration.render();
-
-const profile = new Profile(app);
-profile.render();
+const landing = new Landing(app);
+landing.render();
