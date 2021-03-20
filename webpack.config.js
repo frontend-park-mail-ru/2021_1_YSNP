5377f3360d6d83d74710ea2567b4ec46cb864147
Alekhin Sergey <alekhin1991@gmail.com>
2021-03-20 22:33:33 +0300

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
     entry: './public/main.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     },
     plugins: [
         new MiniCssExtractPlugin({
