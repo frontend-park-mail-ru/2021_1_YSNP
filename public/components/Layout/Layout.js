cae9f5b45ed707fb7acf3ff4f1f19b9dd4bbd39e
Alekhin Sergey <alekhin1991@gmail.com>
2021-03-24 19:11:49 +0300

    add Layout component

---
+++ public/components/Layout/Layout.js
@@ -0,0 +1,35 @@
+import layoutTemplate from './Layout.hbs';
+import './Layout.css';
+
+/***
+ * Layout component
+ */
+export class Layout {
+    /***
+     * Class constructor
+     * @param {HTMLElement} parent - parent element
+     */
+    constructor(parent) {
+        this.__parent = parent;
+    }
+
+    get parent() {
+        return document.getElementById('layout-parent');
+    }
+
+    get leftParent() {
+        return document.getElementById('layout-left-parent');
+    }
+
+    get rightParent() {
+        return document.getElementById('layout-right-parent');
+    }
+
+    /***
+     * Render component
+     * @param context
+     */
+    render(context) {
+        this.__parent.insertAdjacentHTML('beforeend', layoutTemplate(context));
+    }
+}
