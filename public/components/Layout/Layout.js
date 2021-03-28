e9653abd6cf4b677a2108921de7fc5b079411ee9
Alekhin Sergey <alekhin1991@gmail.com>
2021-03-28 03:36:18 +0300

    add layout component

--- public/components/Layout/Layout.js
+++ public/components/Layout/Layout.js
@@ -13,23 +13,102 @@ export class Layout {
         this.__parent = parent;
     }
 
+
+    /***
+     * Get main parent
+     * @returns {Element}
+     */
     get parent() {
-        return document.getElementById('layout-parent');
+        return document
+            .querySelector('[class="layout-inner"]');
     }
 
+    /***
+     * Get left parent
+     * @returns {Element}
+     */
     get leftParent() {
-        return document.getElementById('layout-left-parent');
+        return document
+            .querySelector('[class="layout-left"]');
     }
 
+    /***
+     * Get right parent
+     * @returns {Element}
+     */
     get rightParent() {
-        return document.getElementById('layout-right-parent');
+        return document
+            .querySelector('[class="layout-right"]');
+    }
+
+    /***
+     * Check max width
+     * @private
+     */
+    __checkWidth() {
+        if (this.__context.layoutCount === 'two') {
+            if (this.__context.leftWidth + this.__context.rightWidth > 100) {
+                throw new Error('leftWidth + rightWidth > 100');
+            }
+        }
+    }
+
+    __setWidth(left, right) {
+        this.__context.leftWidth = left;
+        this.__context.rightWidth = right;
+    }
+
+    __getWidth() {
+        if (this.__context.layoutCount === 'one') {
+            return;
+        }
+
+        switch (this.__context.layoutWidth) {
+            case '30/70': {
+                this.__setWidth(30, 70);
+                break;
+            }
+
+            case '40/60': {
+                this.__setWidth(40, 60);
+                break;
+            }
+
+            case '60/40': {
+                this.__setWidth(60, 40);
+                break;
+            }
+
+            case '70/30': {
+                this.__setWidth(70, 30);
+                break;
+            }
+
+            case 'custom': {
+                this.__checkWidth();
+                break;
+            }
+
+            default : {
+                throw new Error('unexpected width');
+            }
+        }
     }
 
     /***
      * Render component
      * @param context
      */
-    render(context) {
-        this.__parent.insertAdjacentHTML('beforeend', layoutTemplate(context));
+    render(context = {
+        layoutCount: 'one'
+    }) {
+        try {
+            this.__context = context;
+            this.__getWidth();
+
+            this.__parent.insertAdjacentHTML('beforeend', layoutTemplate(context));
+        } catch (err) {
+            console.log(err.message);
+        }
     }
 }
