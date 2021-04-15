b7419553b27200bb481198a2974d497be9df34b9
Ivan Gorshkov <ivan.a.gorshkov@gmail.com>
2021-04-16 01:25:16 +0300

    вернул урл на место

--- public/modules/backUrls.js
+++ public/modules/backUrls.js
@@ -6,8 +6,8 @@ class BackUrls {
      * Class constructor
      */
     constructor() {
-        //this.__url = 'https://ykoya.ru';
-        this.__url = 'http://localhost:8080';
+        this.__url = 'https://ykoya.ru';
+        //this.__url = 'http://localhost:8080';
         this.__api = '/api/v1';
     }
 
